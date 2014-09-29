9f3dd3706fc660712086624ee7c1f0e91a030eb6
Claudio Silva <claudio@solee.com.br>
2014-09-29 19:20:43 -0300

    Estrutura Inicial

---
+++ app/services/services.js
@@ -0,0 +1,10 @@
+(function (window) {
+    'use strict';
+
+    window.angular.module('servicesModule', [])
+        .service('tipoImovelService', function () {})
+        .service('finalidadeService', function () {})
+        .service('bairroService', function () {})
+        .service('lancamentoService', function () {});
+    
+}(window));
